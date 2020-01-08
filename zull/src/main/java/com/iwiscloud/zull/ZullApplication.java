2a1280f41a315c7791a251353b67c354bc538469
itheima@itcast.cn <1148299248@qq.com>
2020-01-08 09:59:38 +0800

    1.8修改

--- zull/src/main/java/com/iwiscloud/zull/ZullApplication.java
+++ zull/src/main/java/com/iwiscloud/zull/ZullApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 public class ZullApplication {
 
     public static void main(String[] args) {
+		System.out.println("123");
         SpringApplication.run(ZullApplication.class, args);
     }
 
